e0359d284a0f278f94f0a2011f35a8378259ff4d
Mark Veltzer <mark.veltzer@gmail.com>
2016-10-24 10:56:10 +0300

    move to templar

--- templardefs/deps.py
+++ templardefs/deps.py
@@ -5,7 +5,7 @@ dependencies for this project
 def populate(d):
     d.requirements3=[
         'google-api-python-client',
-        'gdata-python-client',
+        #'gdata-python-client',
     ]
 
 def getdeps():
