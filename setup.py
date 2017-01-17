b53afc6d5c5764d8654a08e9e1bd59c298fe2c2e
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-17 11:33:18 +0200

    start to register module

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+import sys
+if not sys.version_info[0] == 3:
+    sys.exit("Sorry, only python version 3 is supported")
+
+setuptools.setup(
+    name='pycontacts',
+    version='0.0.1',
+    description='pycontacts is a collection of utilities to help interact with google contacts',
+    long_description='pycontacts helps you with various google contacts tasks',
+    url='https://veltzer.github.io/pycontacts',
+    author='Mark Veltzer',
+    author_email='mark.veltzer@gmail.com',
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='google contacts python',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src'),
+    install_requires=[
+        'google-api-python-client',  # main client library
+    ],
+    entry_points={
+        'console_scripts': [
+            'pgc_list=pycontacts.list:main',
+        ],
+    },
+)
