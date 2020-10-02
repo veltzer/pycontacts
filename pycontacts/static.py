7035ad79d3a27e9b07a546e8ffc42cc6941fb2fb
Mark Veltzer <mark.veltzer@gmail.com>
2020-10-02 15:20:03 +0300

    new Makefile

---
+++ pycontacts/static.py
@@ -0,0 +1,4 @@
+""" version which can be consumed from within the module """
+VERSION_STR = "0.0.10"
+DESCRIPTION = "pycontacts is a collection of utilities to help interact with google contacts"
+APP_NAME = "pycontacts"
