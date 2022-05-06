13a10543eb13f0c3e64b7561a9217c5d86767e12
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:11:29 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -6,8 +6,6 @@ console_scripts = [
     "pycontacts=pycontacts.main:main",
 ]
 
-setup_requires = []
-
 run_requires = [
     "httplib2",
     "google-api-python-client",
@@ -33,12 +31,6 @@ dev_requires = [
     "Sphinx",
     "black",
 ]
-
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {
-}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
