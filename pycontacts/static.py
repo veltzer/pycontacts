521393ad6c7adf5850a3a64128a8b9ce4cc6b12c
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:33:35 +0300

    fixed up deps

--- pycontacts/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "0.0.17"
-DESCRIPTION = "Interact with google contacts"
-APP_NAME = "pycontacts"
-LOGGER_NAME = "pycontacts"
